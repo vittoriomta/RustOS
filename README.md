8e515503aa5a7f09b5322aa827e99474ff7ab41e
donut <donut@donuts-Air.lan>
2024-04-17 00:31:40 +0200

    vga text

---
+++ README.md
@@ -0,0 +1,9 @@
+*** Rust OS ***
+
+Just learning Operating System, why not combining with Rust in the mix!
+
+
+Blog guide: https://os.phil-opp.com/minimal-rust-kernel/ . **10/10**
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    loop {}
+}
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    loop {}
-}
-
