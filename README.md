c90cb937b1e527fc98340ea3b15d61fcf16bbc59
donut <donut@donuts-Air.lan>
2024-04-19 20:00:09 +0200

    tests integrations

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-*** Rust OS ***
+### Rust OS ###
 
-Just learning Operating System, why not combining with Rust in the mix!
+Just learning os, why not combining with Rust in the mix!
 
 
-Blog guide: https://os.phil-opp.com/minimal-rust-kernel/ . **10/10**
-
+**Blog guide (10/10 quality):** https://os.phil-opp.com/minimal-rust-kernel/ . 
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
+use rust_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
 }
